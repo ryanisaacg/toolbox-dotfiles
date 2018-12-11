0dd3c76ce0e94967589670d86fb5611ccf292cfa
Ryan Goldstein <ryanisaacg@gmail.com>
2018-12-11 09:53:59 -0500

    Use ripgreg for FZF to get gitignoring

--- setup.fish
+++ setup.fish
@@ -3,3 +3,4 @@ ln -s (pwd)/gitconfig ~/.gitconfig
 ln -s (pwd)/gitignore_global ~/.gitignore_global
 ln -s (pwd)/vimrc ~/.config/nvim/init.vim
 set -U fish_user_paths $fish_user_paths ~/.config/dotfiles/bin ~/.cargo/bin
+set -Ux FZF_DEFAULT_COMMAND "rg -l ."
