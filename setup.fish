5433acd2ccbc98bc18139861e03ac5fb630f9107
Ryan Goldstein <rig24@rutgers.edu>
2018-12-18 12:16:39 -0500

    Set the $EDITOR variable to nvim

--- setup.fish
+++ setup.fish
@@ -4,3 +4,4 @@ ln -s (pwd)/gitignore_global ~/.gitignore_global
 ln -s (pwd)/vimrc ~/.config/nvim/init.vim
 set -U fish_user_paths $fish_user_paths ~/.config/dotfiles/bin ~/.cargo/bin
 set -Ux FZF_DEFAULT_COMMAND "rg -l ."
+set -gx EDITOR nvim
