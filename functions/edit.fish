e6b3923bdc33527ec0a0f513a61e3ca7e46b093a
Ryan Goldstein <rig24@rutgers.edu>
2019-01-30 12:06:05 -0500

    Allow edit to work with vim or nvim

--- functions/edit.fish
+++ functions/edit.fish
@@ -1,6 +1,6 @@
 function edit
-    # Check if there is already a running nvim instance
-    set jobline (jobs | grep 'nvim' -m 1)
+    # Check if there is already a running vim instance
+    set jobline (jobs | grep 'vim' -m 1)
     # Get the job index from the job line
     set job (string match -r '\W+\w*' $jobline)
     if [ "$job" ]
