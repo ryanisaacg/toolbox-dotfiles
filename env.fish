0c700c472fb77e4482c4b332fd2f47ace6912cd3
Ryan Goldstein <rig24@rutgers.edu>
2019-01-30 12:02:28 -0500

    Split env and symlinks into two different scripts

---
+++ env.fish
@@ -0,0 +1,3 @@
+set -U fish_user_paths $fish_user_paths ~/.config/dotfiles/bin ~/.cargo/bin
+set -Ux FZF_DEFAULT_COMMAND "rg -l ."
+set -gx EDITOR vim
--- symlinks.fish
+++ symlinks.fish
@@ -2,6 +2,3 @@ ln -s (pwd)/functions ~/.config/fish/functions
 ln -s (pwd)/gitconfig ~/.gitconfig
 ln -s (pwd)/gitignore_global ~/.gitignore_global
 ln -s (pwd)/vimrc ~/.config/nvim/init.vim
-set -U fish_user_paths $fish_user_paths ~/.config/dotfiles/bin ~/.cargo/bin
-set -Ux FZF_DEFAULT_COMMAND "rg -l ."
-set -gx EDITOR nvim
