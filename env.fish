b60ff694bc47c12c0064f2259f4e4dd77267a0a5
Ryan Goldstein <ryanisaacg@gmail.com>
2019-02-01 15:51:12 -0500

    Add VIMINIT to the fish environment

--- env.fish
+++ env.fish
@@ -1,3 +1,4 @@
 set -U fish_user_paths $fish_user_paths ~/.config/dotfiles/bin ~/.cargo/bin
 set -Ux FZF_DEFAULT_COMMAND "rg -l ."
 set -gx EDITOR vim
+set -Ux VIMINIT 'let $MYVIMRC="~/.config/dotfiles/vimrc" | source $MYVIMRC'
