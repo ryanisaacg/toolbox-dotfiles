05068a15884a3499e65df4aa0fb9506452bc96fd
Ryan Goldstein <ryan@ryanisaacg.com>
2020-02-09 18:11:05 -0500

    By default, don't use a VIMINIT

--- workshop/fish/conf.d/env.fish
+++ workshop/fish/conf.d/env.fish
@@ -3,4 +3,5 @@
 set -x FZF_DEFAULT_COMMAND "rg -l ."
 set -x EDITOR nvim
 #set -x VIMINIT 'let $MYVIMRC="~/.config/dotfiles/vim/vimrc" | source $MYVIMRC'
+set -x VIMINIT ''
 stty start undef stop undef
